c776b56a3616578bb22bb5d8aa98b397fb18db3f
giorgiozoccatelli <giorgio.zoccatelli20@gmail.com>
2025-08-26 19:14:49 +0200

    Mapper migliorato

--- Lab2/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
+++ Lab2/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -1,11 +1,9 @@
 package it.polito.bigdata.hadoop;
 
 import java.io.IOException;
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
+import it.polito.bigdata.hadoop.DriverBigData.COUNTERS;
 
 /**
  * Lab  - Mapper
@@ -13,16 +11,29 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 /* Set the proper data types for the (key,value) pairs */
 class MapperBigData extends Mapper<
-                    LongWritable, // Input key type
+                    Text, // Input key type
                     Text,         // Input value type
                     Text,         // Output key type
-                    IntWritable> {// Output value type
+                    Text> {// Output value type
+    
+    String prefix;
+
+    protected void setup(Context context) throws IOException, InterruptedException {
+    		/* Get the prefix from the configuration */
+        prefix=context.getConfiguration().get("prefix").toString();
+    }
     
     protected void map(
-            LongWritable key,   // Input key type
-            Text value,         // Input value type
-            Context context) throws IOException, InterruptedException {
-
-    		/* Implement the map method */ 
+        Text key,   // Input key type
+        Text value,         // Input value type
+        Context context) throws IOException, InterruptedException {
+            
+        if () {
+            context.write(key, value);
+            context.getCounter(COUNTERS.SELECTED_WORDS).increment(1);
+        }
+        else {
+            context.getCounter(COUNTERS.DISCARDED_WORDS).increment(1);
+        }
     }
 }
