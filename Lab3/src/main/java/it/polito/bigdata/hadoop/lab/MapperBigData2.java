eb9839b80584d94dd624483f849b9e5029865634
giorgiozoccatelli <giorgio.zoccatelli20@gmail.com>
2025-08-27 15:18:32 +0200

    Modificati Mapper e Reducer 2

--- Lab3/src/main/java/it/polito/bigdata/hadoop/lab/MapperBigData2.java
+++ Lab3/src/main/java/it/polito/bigdata/hadoop/lab/MapperBigData2.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -13,16 +14,32 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 /* Set the proper data types for the (key,value) pairs */
 class MapperBigData2 extends Mapper<
-                    LongWritable, // Input key type
-                    Text,         // Input value type
-                    Text,         // Output key type
-                    IntWritable> {// Output value type
+                    Text, // Input key type
+                    IntWritable,         // Input value type
+                    NullWritable,         // Output key type
+                    WordCountWritable> {// Output value type
+
+    private TopKVector<WordCountWritable> localTopK;
+    private Integer k = 100;
+
+    protected void setup(Context context) throws IOException, InterruptedException {
+        localTopK = new TopKVector<WordCountWritable>(k);
+    }
     
     protected void map(
             LongWritable key,   // Input key type
             Text value,         // Input value type
             Context context) throws IOException, InterruptedException {
+            
+            WordCountWritable currentPair = new WordCountWritable(key.toString(), Integer.valueOf((Integer.parseInt(value.toString())))); //Text->String, String->int, int->Integer
+    		localTopK.updateWithNewElement(currentPair);
 
-    		/* Implement the map method */ 
+    }
+    
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        for (WordCountWritable p : localTopK.getLocalTopK()) {
+            context.write(NullWritable.get(), new WordCountWritable(p));
+        }
     }
 }
+
--- Lab3/src/main/java/it/polito/bigdata/hadoop/lab/ReducerBigData2.java
+++ Lab3/src/main/java/it/polito/bigdata/hadoop/lab/ReducerBigData2.java
@@ -3,6 +3,7 @@ package it.polito.bigdata.hadoop.lab;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -12,18 +13,28 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 /* Set the proper data types for the (key,value) pairs */
 class ReducerBigData2 extends Reducer<
-                Text,           // Input key type
-                IntWritable,    // Input value type
+                NullWritable,           // Input key type
+                WordCountWritable,    // Input value type
                 Text,           // Output key type
                 IntWritable> {  // Output value type
     
+    private Integer k =100;
+    
     @Override
     protected void reduce(
-        Text key, // Input key type
-        Iterable<IntWritable> values, // Input value type
+        NullWritable key, // Input key type
+        Iterable<WordCountWritable> values, // Input value type
         Context context) throws IOException, InterruptedException {
 
-		/* Implement the reduce method */
+        TopKVector<WordCountWritable> globTopK = new TopKVector<WordCountWritable>(k);
+
+        for (WordCountWritable currentPair: values ){
+            globTopK.updateWithNewElement(new WordCountWritable(currentPair));
+        }
     	
+        for (WordCountWritable p:globTopK.getLocalTopK()){
+            context.write(new Text(p.getWord()), new IntWritable(p.getCount()));
+        }
+
     }
 }
